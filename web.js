825407e2b48571849e034121f83b5b0aba05c42d
Oliver Staton <oliver.staton@ge.com>
2013-07-18 03:14:04 +0000

    read index - attempt 04

--- web.js
+++ web.js
@@ -6,14 +6,13 @@ fs.readFile("index.html", function (err, data) {
   if (err) {
     throw err;
   }
-});
+  app.get('/', function(request, resonse) {
+    var buf = new Buffer(data);
+    response.send(buf.toString());
+  });
 
-app.get('/', function(request, resonse) {
-  var buf = new Buffer(data);
-  response.send(buf.toString());
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+  var port = process.env.PORT || 5000;
+  app.listen(port, function() {
+    console.log("Listening on " + port);
+  });
 });
