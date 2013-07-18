3f3e0659996bbfa02be708647f744f354ac2a3cf
Oliver Staton <oliver.staton@ge.com>
2013-07-18 03:10:44 +0000

    read index - attempt 03

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var fs = require('fs');
+var express = require('express');
+var app = express.createServer(express.logger());
 
 fs.readFile("index.html", function (err, data) {
   if (err) {
     throw err;
   }
+});
+
+app.get('/', function(request, resonse) {
   var buf = new Buffer(data);
-  console.log(buf.toString());
+  response.send(buf.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
