90f24e7ddd0e80c2ef3055ca7cc91db8a0d70919
Oliver Staton <oliver.staton@ge.com>
2013-07-18 03:35:10 +0000

    read index - attempt 06

--- web.js
+++ web.js
@@ -1,31 +1,30 @@
 var fs = require('fs');
-//var express = require('express');
-//var app = express.createServer(express.logger());
-var fileName = "index.html";
+var express = require('express');
+var app = express.createServer(express.logger());
+//var fileName = "index.html";
 
+/*
 fs.stat(fileName, function(error,stats) {
   
   fs.open(fileName, "r", function(error,fd) {
     var buf = new Buffer(stats.size);
 
     fs.read(fd, buf, 0, buf.length, null, function(error, bytesRead, buf) {
-      var data = buf.toString("utf8", 0, buf.length);
+      var data = buf.toString("utf-8", 0, buf.length);
       console.log(data);
       fs.close(fd);
     });
   });
 });
-
-/*
-fs.readFileSync("index.html", buf) {
-  app.get('/', function(request, resonse) {
-    var buf = new Buffer(data);
-    response.send(buf.toString());
-  });
-
-  var port = process.env.PORT || 5000;
-  app.listen(port, function() {
-    console.log("Listening on " + port);
-  });
-});
 */
+
+app.get('/', function(request, resonse) {
+//  var buf = new Buffer(data);
+  response.send("Trial Run");
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
