2bbc6e8d7ee42ceaf9df091d6e06a5fd0c835bbd
Oliver Staton <oliver.staton@ge.com>
2013-07-18 02:38:25 +0000

    read index.html - attempt 01

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hey Soumya!');
+var content;
+fs.readFile('./index.html', function read(err, data)
+{
+  if(err)
+  {
+    throw err;
+  }
+  content = data;
 });
+console.log (content);
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+#var express = require('express');
+#
+#var app = express.createServer(express.logger());
+#
+#app.get('/', function(request, response) {
+#  response.send('Hey Soumya!');
+#});
+#
+#var port = process.env.PORT || 5000;
+#app.listen(port, function() {
+#  console.log("Listening on " + port);
+#});
