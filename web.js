fa76a7a1714528e61775772f4265ff177f67d885
Oliver Staton <oliver.staton@ge.com>
2013-07-18 03:42:37 +0000

    read index - attempt 08

--- web.js
+++ web.js
@@ -1,30 +1,28 @@
-//var fs = require('fs');
+var fs = require('fs');
 var express = require('express');
 var app = express.createServer(express.logger());
-//var fileName = "index.html";
+var fileName = "index.html";
 
-/*
 fs.stat(fileName, function(error,stats) {
   
   fs.open(fileName, "r", function(error,fd) {
+
     var buf = new Buffer(stats.size);
 
     fs.read(fd, buf, 0, buf.length, null, function(error, bytesRead, buf) {
-      var data = buf.toString("utf-8", 0, buf.length);
-      console.log(data);
+      
+      app.get('/', function(request,response) {
+        var data = buf.toString("utf-8", 0, buf.length);
+        response.send(data);
+      });
+
+      var port = process.env.PORT || 5000;
+
+      app.listen(port,function() {
+        console.log("Listening on " + port);
+      });
+
       fs.close(fd);
     });
   });
 });
-*/
-
-app.get('/', function(request, response) {
-//  var buf = new Buffer(data);
-  response.send("Trial Run");
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
